83a662bb09ffd321fb312ed0311f2232220860bf
spongeboi <hi@spongeboi.com>
2024-10-10 11:22:06 -0700

    add bluetooth support

--- doomsday-messenger.ino
+++ doomsday-messenger.ino
@@ -1,15 +1,30 @@
 #include <SPI.h>
 #include <LoRa.h>
+#include <BluetoothSerial.h>
 
-//define the pins used by the transceiver module
 #define ss 5
 #define rst 14
 #define dio0 2
 
+BluetoothSerial SerialBT;
+
 byte msgCount = 0;            // count of outgoing messages
 long lastSendTime = 0;        // last send time
-int interval = 2000;          // interval between sends
+int interval = 200;          // interval between sends
+byte address = 0x01;
 
+String readBtMsg() {
+  String text = SerialBT.readStringUntil('\n');
+  text.remove(text.length()-1,1);
+  return text;
+}
+
+void sendBtMsg(String msg) {
+  uint8_t buf[msg.length()];
+  memcpy(buf,msg.c_str(),msg.length());
+  SerialBT.write(buf, msg.length());
+  SerialBT.println();
+}
 
 void setup() {
   //initialize Serial Monitor
@@ -33,11 +48,12 @@ void setup() {
   // ranges from 0-0xFF
   LoRa.setSyncWord(0xF3);
   Serial.println("LoRa Initializing OK!");
+  SerialBT.begin("Doomsday-messenger-1");
 }
 
 void loop() {
-  if (Serial.available()) {
-    String inputStr = Serial.readString();
+  if (SerialBT.available()) {
+    String inputStr = readBtMsg();
     inputStr.trim();
     if (inputStr.length() > 0) {
       if (millis() - lastSendTime > interval) {
@@ -55,6 +71,7 @@ void loop() {
 
 void sendMessage(String outgoing) {
   LoRa.beginPacket();                   // start packet
+  LoRa.write(address);                  // add message sender
   LoRa.write(msgCount);                 // add message ID
   LoRa.write(outgoing.length());        // add payload length
   LoRa.print(outgoing);                 // add payload
@@ -64,6 +81,7 @@ void sendMessage(String outgoing) {
 
 void onReceive(int packetSize){
   if (packetSize == 0) return;          // if there's no packet, return
+  byte senderAddr = LoRa.read();
   byte incomingMsgId = LoRa.read();     // incoming msg ID
   byte incomingLength = LoRa.read();    // incoming msg length
 
@@ -75,14 +93,19 @@ void onReceive(int packetSize){
 
   if (incomingLength != incoming.length()) {   // check length for error
     Serial.println("error: message length does not match length");
+    Serial.println(incoming);
     return;                             // skip rest of function
   }
 
   Serial.println("Recieved Message");
+  Serial.print("Recieved from: ");
+  Serial.print(senderAddr, HEX);
+  Serial.print("\n");
   Serial.println("Message ID: " + String(incomingMsgId));
   Serial.println("Message length: " + String(incomingLength));
   Serial.println("Message: " + incoming);
   Serial.println("RSSI: " + String(LoRa.packetRssi()));
   Serial.println("Snr: " + String(LoRa.packetSnr()));
   Serial.println();
+  sendBtMsg("Message Recieved: \n Sender: " + String(senderAddr) + "\n" + incoming);
 }
